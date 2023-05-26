dfdef75f521cbeeb3bd11c7e5c9105c9b512aeb0
Tyrannoeil <114586492+Tyrannoeil@users.noreply.github.com>
2023-05-26 02:21:44 -0400

    Create module.js

---
+++ module.js
@@ -0,0 +1,18 @@
+// Importer les scripts supplémentaires ici
+import diceRoll from './scripts/diceRoll.js';
+import popup from './scripts/popup.js';
+
+// Exécuter les initialisations ou configurations nécessaires
+diceRoll.initialize();
+popup.initialize();
+
+// Exporter les modules ou fonctionnalités nécessaires
+export default {
+  init() {
+    // Initialisation du module
+  },
+  ready() {
+    // Actions à effectuer lorsque le système est prêt
+  },
+  // Autres fonctions et modules personnalisés
+};
